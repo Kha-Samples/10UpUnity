397cd49e9c4b8b8f580689517a03d6cf9ac91d75
Ingo Jakobs <ingo.jakobs@stud.tu-darmstadt.de>
2015-01-24 21:32:22 +0100

    text + door update

--- Sources/localization/Keys_text.hx
+++ Sources/localization/Keys_text.hx
@@ -5,6 +5,10 @@ class Keys_text {
 	static public var PROFESSOR = "Professor";
 	static public var FISCHMENSCH = "Fischmensch";
 	static public var CLICK_TO_START = "click_to_start";
+	static public var START_AS_MECHANIC = "start_as_mechanic";
+	static public var START_AS_BULLY_1 = "start_as_bully_1";
+	static public var START_AS_BULLY_2 = "start_as_bully_2";
+	static public var START_AS_BULLY_3 = "start_as_bully_3";
 	static public var ABILITY_PUNCH = "ability_Punch";
 	static public var ABILITY_LIFT = "ability_Lift";
 	static public var ABILITY_DANCE = "ability_Dance";
